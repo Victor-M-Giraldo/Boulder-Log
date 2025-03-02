a832c6447446945f93dfaea8a5a67433c3b9a27f
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-03-01 22:21:01 -0500

    Refactor ClimbService to include GetClimbs function and improve formatting; update UserService to set Content-Type header for POST requests

--- client/src/services/ClimbService.ts
+++ client/src/services/ClimbService.ts
@@ -1,17 +1,34 @@
-    import { getItem } from '../utils/localStorage';
-    import { Token } from '../types/api';
-    import { ApiClient } from '../utils/ApiClient';
+import { getItem } from '../utils/localStorage';
+import { Token } from '../types/api';
+import { ApiClient } from '../utils/ApiClient';
 
-    export async function CreateClimb(formData: FormData) {
-        const tokenData = getItem<Token>('token');
-        if (!tokenData || !tokenData.token) {
-            return;
-        }
-        const { token } = tokenData;
-        return ApiClient('climbs', {
-            headers: {
-                Authorization: token,
-            },
-            method: 'POST',
-        }, formData);
-    }
+export async function CreateClimb(formData: FormData) {
+  const tokenData = getItem<Token>('token');
+  if (!tokenData || !tokenData.token) {
+    return;
+  }
+  const { token } = tokenData;
+  return ApiClient(
+    'climbs',
+    {
+      headers: {
+        Authorization: token,
+      },
+      method: 'POST',
+    },
+    formData
+  );
+}
+export async function GetClimbs() {
+  const tokenData = getItem<Token>('token');
+  if (!tokenData || !tokenData.token) {
+    return;
+  }
+  const { token } = tokenData;
+  const res = await ApiClient('climbs', {
+    headers: {
+      Authorization: token,
+    },
+  });
+  return await res.json();
+}
