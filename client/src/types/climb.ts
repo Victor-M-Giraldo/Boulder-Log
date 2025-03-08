f4f11e237618c3dea62511d15229504bef42c028
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-03-08 15:12:28 -0500

    Update Climb interface to use Note type for notes and include notes in user climbs retrieval

--- client/src/types/climb.ts
+++ client/src/types/climb.ts
@@ -1,3 +1,5 @@
+import { Note } from "./note";
+
 export type GradeOptions = "V0" | "V1" | "V2" | "V3" | "V4" | "V5" | "V6" | "V7" | "V8" | "V9" | "V10" | "V11" | "V12";
 
 export interface Climb {
@@ -6,6 +8,6 @@ export interface Climb {
     location: string;
     createdAt: string;
     completed: boolean;
-    notes: string[];
+    notes: Note[];
     imageUrl: string;
 }
