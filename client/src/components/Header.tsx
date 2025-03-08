807f98b47226bff7076ce22916cc4f27537318e5
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-03-08 15:12:19 -0500

    Add user authentication handling in Header and RegistrationPage components

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,25 +1,36 @@
-import { Link } from "react-router";
+import { Link } from 'react-router';
+import useUser from '../hooks/useUser';
 
 export default function Header() {
-    return (
-      <>
-        <header className='p-4 bg-base-100'>
-          <div className='max-w-7xl flex m-auto'>
-            <h1 className='text-3xl'>
-              <Link to='/'>Boulder Log</Link>
-            </h1>
-            <nav className='ml-auto flex items-center'>
-              <ul className='flex gap-2'>
-                <a href='' className='hover:link'>
+  const { user } = useUser();
+  return (
+    <>
+      <header className='p-4 bg-base-100'>
+        <div className='max-w-7xl flex m-auto'>
+          <h1 className='text-3xl'>
+            <Link to='/'>Boulder Log</Link>
+          </h1>
+          <nav className='ml-auto flex items-center'>
+            <ul className='flex gap-2'>
+              {user && (
+                <Link to='/climbs' className='hover:link'>
                   <li>View Climbs</li>
-                </a>
-                <Link to='/login' className='hover:link'>
-                  <li>Log In</li>
                 </Link>
-              </ul>
-            </nav>
-          </div>
-        </header>
-      </>
-    );
+              )}
+              {!user && (
+                <>
+                  <Link to='/login' className='hover:link'>
+                    <li>Log In</li>
+                  </Link>
+                  <Link to='/register' className='hover:link'>
+                    <li>Register</li>
+                  </Link>
+                </>
+              )}
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
--- client/src/pages/RegistrationPage.tsx
+++ client/src/pages/RegistrationPage.tsx
@@ -1,13 +1,27 @@
-import AsideImage from "../components/AsideImage";
-import RegistrationForm from "../components/RegistrationForm";
+import AsideImage from '../components/AsideImage';
+import RegistrationForm from '../components/RegistrationForm';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import useUser from '../hooks/useUser';
 
 export default function RegistrationPage() {
+  const navigate = useNavigate();
+  const { user } = useUser();
+  useEffect(() => {
+    if (user) {
+      navigate('/climbs');
+    }
+  }, [user, navigate]);
   return (
-    <section className='h-full flex items-center justify-center p-6'>
-      <div className='bg-base-100 rounded-lg shadow-md grid md:grid-cols-2 w-full max-w-4xl'>
-        <AsideImage />
-        <RegistrationForm/>
-      </div>
-    </section>
+    <>
+      {!user && (
+        <section className='h-full flex items-center justify-center p-6'>
+          <div className='bg-base-100 rounded-lg shadow-md grid md:grid-cols-2 w-full max-w-4xl'>
+            <AsideImage />
+            <RegistrationForm />
+          </div>
+        </section>
+      )}
+    </>
   );
-};
+}
